a4d248c3894dd11f43a37098e86efe3ba426041c
DangaRanga <jasonblue1626@gmail.com>
2020-10-09 22:40:40 -0500

    Added buggy python

---
+++ BuggyPython/broken_syntax.py
@@ -0,0 +1,25 @@
+# Level 1 in the Python series of Buggy code for inter clubbing
+
+def prob_1():
+    n = (3)
+    for num in n:
+        print(num)
+
+def lambda_array():
+    # initialize an empty array
+    lambda_methods = {}
+    # implement a for loop to count from 0 to 9
+    for i in 10:
+        # append the lambda function to the array defined above
+        lambdamethods.push(lambda x: x + i)
+
+    return lambda_methods
+
+
+def linearsearch(arr, x):
+    for i in range(length(arr)):
+        if arr[i] = x:
+            return i
+    return -1
+
+
